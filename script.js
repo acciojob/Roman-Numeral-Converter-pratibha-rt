1ad28d0aa58af4cb7d920720997108ae0b202d48
pratibha-rt <pratibha.sept19@gmail.com>
2025-08-28 18:14:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,45 +1,33 @@
 function convertToRoman(num) {
-  	const obj = {
-      0:['M',1000], 
-      1:['D', 500], 
-      2:['C', 100], 
-      3:['L', 50], 
-      4:['X', 10], 
-      5:['V', 5], 
-      6:['I', 1]
-    };
+  	const sym = [
+        ["M", 1000],
+        ["CM", 900],
+        ["D", 500],
+        ["CD", 400],
+        ["C", 100],
+        ["XC", 90],
+        ["L", 50],
+        ["XL", 40],
+        ["X", 10],
+        ["IX", 9],
+        ["V", 5],
+        ["IV", 4],
+        ["I", 1]
+    ];
 	let result = "";
-	for (int i = 0; i <= 6; i ++) {
-		for (let i = 0; i <= 6; i ++) {
-			let symbol = obj[i][0];
-			let value = obj[i][1];
 
-			if (i % 2 === 0 && i < 6) {
-				let nextValue = obj[i][1];
-			 let subtractValue = obj[i + 2][1];
-				let subtractSymbol = obj[i + 2][0];
-
-				if (num >= nextValue - subtractValue) {
-                result += subtractSymbol + symbol;
-                num -= (nextValue - subtractValue);
-            }
-			}
-
-			 while (num >= value) {
-            result += symbol;
-            num -= value;
+	sym.forEach(item => {
+        const [key, val] = item;
+        while (num >= val) {
+            num -= val;
+            result += key;
         }
-		}
+    });
 
-		console.log(result);
-	}
+    return result;
 
 }
 
 console.log(convertToRoman(36));
 
 module.exports = convertToRoman
-
-
-
-
